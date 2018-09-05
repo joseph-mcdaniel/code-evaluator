dc48179672e3d65e056675b8aa611b16cae0eb0e
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2018-09-05 13:07:25 -0700

    containerize flask app

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,10 +1,14 @@
+import sys
+import os
 from flask import Flask, jsonify
+
 
 # instatiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
